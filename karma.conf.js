6af5a1bbff32c94c77c9692d3c68c671a5315a75
Robin Böhm <robinboehm@googlemail.com>
2013-09-27 14:23:30 +0200

    Add inital test env

---
+++ karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: ['jasmine'],
+        files     : [
+            //libs
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            // src
+            'src/**.js',
+            /* Match all test files */
+            'test/**/*.js'
+        ],
+        reporters : ['dots'],
+        logLevel  : config.LOG_WARN,
+        browsers  : ['PhantomJS'],
+        singleRun : true
+    });
+};
