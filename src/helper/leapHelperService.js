4c879f5bafee83f3fe7a0882cd90c68f64f3eea4
Robin Böhm <robinboehm@googlemail.com>
2013-09-29 01:31:30 +0200

    add implementation for leapHelperService.timeout

---
+++ src/helper/leapHelperService.js
@@ -0,0 +1,22 @@
+angular.module('angular-leap').service("leapHelperService", [
+    '$timeout', function ($timeout) {
+        // TODO: Make Configurable
+        var timeOut = 650;
+        var timeoutActive = false;
+
+
+        var timeoutHandler = function (ms) {
+            var beforeState = timeoutActive;
+            if (!timeoutActive && ms) {
+                timeoutActive = true;
+                $timeout(function () {
+                    timeoutActive = false;
+                }, timeOut);
+            }
+            return beforeState;
+        };
+
+        return {
+            timeout         : timeoutHandler
+        }
+    }]);
