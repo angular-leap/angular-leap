6c2e880a6fde221e5024636d1e99e0f1bff9cc09
Robin Böhm <robinboehm@googlemail.com>
2013-09-29 01:30:32 +0200

    add test for helperService.timeout logic

--- test/helper/leapHelperService.spec.js
+++ test/helper/leapHelperService.spec.js
@@ -5,4 +5,27 @@ describe("A leapHelperService", function() {
         expect(leapHelperService.timeout).toBeDefined();
     }));
 
+    describe("timeout function", function() {
+
+        it("should return false if no timeout is active", inject(function(leapHelperService,$timeout) {
+            expect(leapHelperService.timeout()).toBe(false);
+        }));
+
+        // TODO: Function not so clear, rename/refactor
+        it("should return current active state on call", inject(function(leapHelperService,$timeout) {
+            expect(leapHelperService.timeout()).toBe(false);
+            expect(leapHelperService.timeout(100)).toBe(false);
+            expect(leapHelperService.timeout(100)).toBe(true);
+            expect(leapHelperService.timeout()).toBe(true);
+            $timeout.flush();
+            expect(leapHelperService.timeout()).toBe(false);
+            expect(leapHelperService.timeout(100)).toBe(false);
+            expect(leapHelperService.timeout(100)).toBe(true);
+            expect(leapHelperService.timeout()).toBe(true);
+        }));
+    })
+
+
+
+
 });
