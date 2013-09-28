60f3115c25c5444630ed2fd69c123010b62ceac2
Robin Böhm <robinboehm@googlemail.com>
2013-09-29 00:25:16 +0200

    modify the mather rules for module files prio

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,8 @@ module.exports = function (config) {
             'bower_components/angular/angular.js',
             'bower_components/angular-mocks/angular-mocks.js',
             // src
-            'src/**.js',
+            'src/**/module.js',
+            'src/**/*.js',
             /* Match all test files */
             'test/**/*.js'
         ],
