600f1fb3cc42c2545c8223a9d536f8d4b40c49af
Robin Böhm <robinboehm@googlemail.com>
2013-09-29 01:11:44 +0200

    Add test for leapHelperService .timeout

--- test/helper/leapHelperService.spec.js
+++ test/helper/leapHelperService.spec.js
@@ -1,3 +1,8 @@
 describe("A leapHelperService", function() {
     beforeEach(module('angular-leap'));
+
+    it("should offer a timeout function", inject(function(leapHelperService) {
+        expect(leapHelperService.timeout).toBeDefined();
+    }));
+
 });
