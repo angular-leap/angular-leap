3542a9249873eca5c41f1d90e0088e5f76509022
Robin Böhm <robinboehm@googlemail.com>
2013-09-29 18:29:45 +0200

    Define testsuite for swipe directive

---
+++ src/directives/swipeEvents.js
@@ -0,0 +1,26 @@
+// Helper Methods
+
+// Directive generator
+var eventDirectives = {};
+angular.forEach(
+    'Left Right Up Down'.split(' '),
+    function (direction) {
+        var directiveName = 'leapSwipe' + direction;
+        eventDirectives[directiveName] = [
+            '$parse', 'leap', 'leapHelperService', function ($parse, leap, leapHelperService) {
+                return function (scope, element, attr) {
+                    var fn = $parse(attr[directiveName]);
+                    leap.controller().on('gesture', function (gesture) {
+                        if (!leapHelperService.timeout(650) && gesture.type === "swipe" && leapHelperService.testForDirection(gesture,direction)) {
+                            scope.$apply(function () {
+                                fn(scope, {$gesture: gesture});
+                            });
+                        }
+                    });
+                };
+            }];
+    }
+);
+
+angular.module('angular-leap').
+    directive(eventDirectives);
--- test/directives/swipeEvents.spec.js
+++ test/directives/swipeEvents.spec.js
@@ -1,5 +1,128 @@
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
-    });
+describe("A swipe gesture directive", function () {
+
+    beforeEach(module("angular-leap"));
+
+    var directions = ["left", "right", "up", "down"];
+
+    var _Leap;
+    var _LeapController;
+
+    beforeEach(inject(function ($window) {
+        _LeapController = function(){};
+        _LeapController.prototype.connect = function(){};
+        _LeapController.prototype.on = function(){};
+
+
+        _Leap = function () {};
+        _Leap.Controller = _LeapController;
+
+        $window.Leap = _Leap;
+    }));
+
+    it("should be executed and register gesture event listener ", inject(function ($rootScope, $compile) {
+        spyOn(_LeapController.prototype,"connect");
+        spyOn(_LeapController.prototype,"on");
+
+        var html = "<div leap-swipe-" + directions[0] + "='test=test+1'></div>";
+        var scope = $rootScope.$new();
+        var element = $compile(html)(scope);
+
+        expect(_LeapController.prototype.connect).toHaveBeenCalled();
+        expect(_LeapController.prototype.on).toHaveBeenCalled();
+        expect(_LeapController.prototype.on.calls[0].args[0]).toEqual("gesture");
+    }));
+
+
+    it("should be executed and register gesture event listener ", inject(function ($rootScope, $compile) {
+        spyOn(_LeapController.prototype,"on");
+
+
+        var html = "<div leap-swipe-" + directions[0] + "='test=test+1'></div>";
+        var scope = $rootScope.$new();
+        var element = $compile(html)(scope);
+
+
+        spyOn(scope,"$apply");
+        // Simulate Swipe Event, execute function
+        _LeapController.prototype.on.calls[0].args[1]({
+            type: "swipe",
+            direction:[1,0]
+        });
+
+        expect(scope.$apply).toHaveBeenCalled();
+    }));
+
+
+    it("should use the timeout function to prevent unwanted double event triggers", inject(function ($rootScope, $compile,$timeout) {
+        spyOn(_LeapController.prototype,"on");
+
+
+        var html = "<div leap-swipe-" + directions[0] + "='test=test+1'></div>";
+        var scope = $rootScope.$new();
+        var element = $compile(html)(scope);
+
+
+        spyOn(scope,"$apply");
+        // Simulate Swipe Event, execute function
+        _LeapController.prototype.on.calls[0].args[1]({
+            type: "swipe",
+            direction:[1,0]
+        });
+        _LeapController.prototype.on.calls[0].args[1]({
+            type: "swipe",
+            direction:[1,0]
+        });
+
+        expect(scope.$apply.calls.length).toBe(1);
+
+
+        $timeout.flush()
+
+        _LeapController.prototype.on.calls[0].args[1]({
+            type: "swipe",
+            direction:[1,0]
+        });
+
+        expect(scope.$apply.calls.length).toBe(2);
+    }));
+
+
+
+    it("should execute expressions", inject(function ($rootScope, $compile) {
+        spyOn(_LeapController.prototype,"on");
+
+        var html = "<div leap-swipe-" + directions[0] + "='test=test+1'></div>";
+        var scope = $rootScope.$new();
+        var element = $compile(html)(scope);
+
+        scope.test = 1;
+
+        _LeapController.prototype.on.calls[0].args[1]({
+            type: "swipe",
+            direction:[1,0]
+        });
+
+        expect(scope.test).toBe(2);
+    }));
+
+    it("should execute scope function", inject(function ($rootScope, $compile) {
+        var scope = $rootScope.$new();
+        scope.testFunction = function(){};
+        spyOn(scope,"testFunction");
+        spyOn(_LeapController.prototype,"on");
+
+        var html = "<div leap-swipe-" + directions[0] + "='testFunction()'></div>";
+        var element = $compile(html)(scope);
+
+        _LeapController.prototype.on.calls[0].args[1]({
+            type: "swipe",
+            direction:[1,0]
+        });
+
+        expect(scope.testFunction).toHaveBeenCalled();
+    }));
+
+
+
+
 });
