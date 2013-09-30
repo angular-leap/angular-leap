0d56c8211271dd6ca4034835d42f000ffb38d59b
Robin Böhm <robinboehm@googlemail.com>
2013-09-30 07:15:44 +0200

    add test for defaultGestureIntense

--- test/config/leapConfigValue.spec.js
+++ test/config/leapConfigValue.spec.js
@@ -9,4 +9,12 @@ describe("A leapConfig Value", function () {
         expect(leapConfig.defaultTimeout).toBe(650);
     }));
 
+
+    it("should contain a defaultTimeout value", inject(function (leapConfig) {
+        expect(leapConfig.defaultGestureIntense).toBeDefined();
+    }));
+
+    it("should contain a defaultTimeout value of 650", inject(function (leapConfig) {
+        expect(leapConfig.defaultGestureIntense).toBe(0.5);
+    }));
 });
