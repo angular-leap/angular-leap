74b84727595e7a83813aa717aacd2536771df4f1
Pascal Precht <pascal.precht@googlemail.com>
2013-11-05 13:08:59 +0100

    chore(jshint): adds jshint support
    this commits adds a valide jshint file and introduces
    the grunt-contrib-jshint plugin to make use of it.
    it also updates all source files accordingly;
    seriously, there weren't any conventions considered
    yet.

--- src/gestures/gestureEvents.js
+++ src/gestures/gestureEvents.js
@@ -1,37 +1,40 @@
 'use strict';
 angular.forEach([
-    'leapKeyTap',
-    'leapScreenTap',
-    'leapCircle',
-    'leapSwipeLeft',
-    'leapSwipeRight',
-    'leapSwipeUp',
-    'leapSwipeDown'],
-    function (directiveName) {
-        var splitByCamelCase = directiveName.replace(/([A-Z])/g, ' $1').split(' '),
-            eventType = splitByCamelCase[1].toLowerCase(),
-            direction = splitByCamelCase[2];
-        // e.g. ScreenTap + KeyTap
-        if (splitByCamelCase[2] === 'Tap') {
-            eventType += splitByCamelCase[2];
-        }
-        angular.module('angular-leap').directive(directiveName, function ($parse, leap, leapHelperService, leapConfig) {
-            return function (scope, element, attr) {
-                var fn = $parse(attr[directiveName]);
-                var timeout = (attr.leapTimeout) ? attr.leapTimeout : leapConfig.defaultTimeout;
-                leap.controller().on('gesture', function (gesture) {
-                    if (gesture.type === eventType) {
-                        if (eventType === 'swipe' && !leapHelperService.testForDirection(gesture, direction)) {
-                            return;
-                        }
-                        if (!leapHelperService.timeout(timeout)) {
-                            scope.$apply(function () {
-                                fn(scope, {$gesture: gesture});
-                            });
-                        }
+  'leapKeyTap',
+  'leapScreenTap',
+  'leapCircle',
+  'leapSwipeLeft',
+  'leapSwipeRight',
+  'leapSwipeUp',
+  'leapSwipeDown'
+], function (directiveName) {
+    var splitByCamelCase = directiveName.replace(/([A-Z])/g, ' $1').split(' '),
+        eventType = splitByCamelCase[1].toLowerCase(),
+        direction = splitByCamelCase[2];
 
-                    }
-                });
-            };
+    // e.g. ScreenTap + KeyTap
+    if (splitByCamelCase[2] === 'Tap') {
+      eventType += splitByCamelCase[2];
+    }
+
+    angular.module('angular-leap').directive(directiveName, function ($parse, leap, leapHelperService, leapConfig) {
+      return function (scope, element, attr) {
+        var fn = $parse(attr[directiveName]);
+        var timeout = (attr.leapTimeout) ? attr.leapTimeout : leapConfig.defaultTimeout;
+
+        leap.controller().on('gesture', function (gesture) {
+          if (gesture.type === eventType) {
+            if (eventType === 'swipe' && !leapHelperService.testForDirection(gesture, direction)) {
+              return;
+            }
+            if (!leapHelperService.timeout(timeout)) {
+              scope.$apply(function () {
+                fn(scope, {$gesture: gesture});
+              });
+            }
+          }
         });
+      };
     });
+  }
+);
--- src/module.js
+++ src/module.js
@@ -1,2 +1,2 @@
 'use strict';
-angular.module("angular-leap", []);
+angular.module('angular-leap', []);
