efccf6605d49486ed7a6b5525939c7ce5e3afc12
Pascal Precht <pascal.precht@googlemail.com>
2013-11-07 13:10:51 +0100

    chore(docs): improvies module docs

--- src/module.js
+++ src/module.js
@@ -4,6 +4,13 @@
  * @name angular-leap
  *
  * @description
- * The main module which holds everything together.
+ * This is the angular-leap main module. To use angular-leap in your angular app simply
+ * declare that your app depends on this module like this:
+ *
+ * <pre>
+ * angular.module('yourApp', ['angular-leap']);
+ * </pre>
+ *
+ * Once this is done you can use all provided components by angular-leap.
  */
 angular.module('angular-leap', []);
