3f1e0fdd07bcbdef4534af92fa00a049ffd40bf4
Robin Böhm <robinboehm@googlemail.com>
2013-12-18 12:39:52 +0100

    refactor(leapService): provide timeout and gestureIntense via leapProvider and Service

--- src/leap.js
+++ src/leap.js
@@ -1,21 +1,49 @@
 'use strict';
 
-angular.module('angularLeap').service('leap', function ($window) {
-  if (!$window.Leap) {
-    throw new Error('You should include LeapJS Native JavaScript API');
-  }
+angular.module('angularLeap')
+    .provider('leap', function () {
 
-  var controller;
+        var timeOutFn,
+            gestureIntenseFn,
+            // Defaults
+            _timeout = 650,
+            _gestureIntense = 0.5;
 
-  var getController = function () {
-    if (!controller) {
-      controller = new $window.Leap.Controller({enableGestures: true});
-      controller.connect();
-    }
-    return controller;
-  };
+        this.timeout = timeOutFn = function (timeout) {
+            if (timeout) {
+                _timeout = timeout;
+            }
+            return _timeout;
+        };
 
-  return {
-    controller: getController
-  };
-});
+        this.gestureIntense = gestureIntenseFn = function (gestureIntense) {
+            if (gestureIntense) {
+                _gestureIntense = gestureIntense;
+            }
+            return _gestureIntense;
+        };
+
+
+        this.$get = function ($window) {
+            if (!$window.Leap) {
+                throw new Error('You should include LeapJS Native JavaScript API');
+            }
+
+            var controller;
+
+            var getController = function () {
+                if (!controller) {
+                    controller = new $window.Leap.Controller({enableGestures: true});
+                    controller.connect();
+                }
+                return controller;
+            };
+
+            return {
+                controller: getController,
+                timeout: timeOutFn,
+                gestureIntense: gestureIntenseFn
+            };
+        };
+
+    });
--- test/leap.spec.js
+++ test/leap.spec.js
@@ -1,75 +1,169 @@
-describe("A leapService", function () {
+describe('A leapService', function () {
 
-    beforeEach(module("angularLeap"));
+    /* Test basic configurations */
+    var testBasicConfiguration = {
+        timeout: 650,
+        gestureIntense: 0.5
+    };
 
-    it("should throw an exception if global variant Leap is not defined", inject(function ($injector) {
+    describe('default instance', function () {
+        beforeEach(module('angularLeap'));
 
-        var invokeLeapService = function () {
-            $injector.invoke(function (leap) {
-            });
-        }
-        expect(invokeLeapService).toThrow(new Error("You should include LeapJS Native JavaScript API"));
-    }));
+        it('should throw an exception if global variant Leap is not defined', inject(function ($injector, $window) {
 
-    describe("that could use global Leap Variant", function () {
+            var invokeLeapService = function () {
+                $injector.invoke(function (leap) {
+                });
+            };
 
-
-        var _Leap;
-        var _LeapController;
-
-        beforeEach(inject(function ($window) {
-            _LeapController = function(){};
-            _LeapController.prototype.connect = function(){};
-
-
-            _Leap = function () {};
-            _Leap.Controller = _LeapController;
-
-            $window.Leap = _Leap;
+            $window.Leap = undefined;
+            expect($window.Leap).not.toBeDefined();
+            expect(invokeLeapService).toThrow(new Error('You should include LeapJS Native JavaScript API'));
         }));
 
-
-        it("should offer a controller function", inject(function ($injector) {
-
-            var leapController;
-            $injector.invoke(function (leap) {
-                leapController = leap;
-            });
-
-            expect(leapController.controller).toBeDefined();
-        }));
+        describe('that could use global Leap Variant', function () {
 
 
-        it("should call LeapController.connect on call of controller", inject(function ($injector) {
-            var leapController;
-            spyOn(_LeapController.prototype,"connect");
+            var _Leap;
+            var _LeapController;
 
-            $injector.invoke(function (leap) {
-                leapController = leap;
-            });
-
-            leapController.controller();
-
-            expect(_LeapController.prototype.connect).toHaveBeenCalled();
-        }));
+            beforeEach(inject(function ($window) {
+                _LeapController = function () {
+                };
+                _LeapController.prototype.connect = function () {
+                };
 
 
-        it("should call LeapController.connect only once", inject(function ($injector) {
-            var leapController;
-            spyOn(_LeapController.prototype,"connect");
+                _Leap = function () {
+                };
+                _Leap.Controller = _LeapController;
 
-            $injector.invoke(function (leap) {
-                leapController = leap;
-            });
-
-            leapController.controller();
-            leapController.controller();
-            leapController.controller();
-
-            expect(_LeapController.prototype.connect.calls.length).toBe(1);
-        }));
+                $window.Leap = _Leap;
+            }));
 
 
+            it('should offer a controller function', inject(function ($injector) {
+
+                var leapController;
+                $injector.invoke(function (leap) {
+                    leapController = leap;
+                });
+
+                expect(leapController.controller).toBeDefined();
+            }));
+
+
+            it('should call LeapController.connect on call of controller', inject(function ($injector) {
+                var leapController;
+                spyOn(_LeapController.prototype, 'connect');
+
+                $injector.invoke(function (leap) {
+                    leapController = leap;
+                });
+
+                leapController.controller();
+
+                expect(_LeapController.prototype.connect).toHaveBeenCalled();
+            }));
+
+
+            it('should call LeapController.connect only once', inject(function ($injector) {
+                var leapController;
+                spyOn(_LeapController.prototype, 'connect');
+
+                $injector.invoke(function (leap) {
+                    leapController = leap;
+                });
+
+                leapController.controller();
+                leapController.controller();
+                leapController.controller();
+
+                expect(_LeapController.prototype.connect.calls.length).toBe(1);
+            }));
+        });
+
+        describe('configuration', function () {
+
+
+            var _Leap;
+            var _LeapController;
+
+            beforeEach(inject(function ($window) {
+                _LeapController = function () {
+                };
+                _LeapController.prototype.connect = function () {
+                };
+
+
+                _Leap = function () {
+                };
+                _Leap.Controller = _LeapController;
+
+                $window.Leap = _Leap;
+            }));
+
+            for (var key in testBasicConfiguration) {
+                if (testBasicConfiguration.hasOwnProperty(key)) {
+                    var value = testBasicConfiguration[key];
+
+                    describe('key: ' + key, function () {
+                        it('should define a default ' + key + ' function', inject(function (leap) {
+                            expect(leap[key]).toBeDefined();
+                        }));
+
+                        it('should define a default ' + key + ' at ' + value, inject(function (leap) {
+                            expect(leap[key]()).toBe(value);
+                        }));
+
+                        it('should define a getter/setter', inject(function (leap) {
+                            var tempValue = leap[key]();
+                            expect(leap[key]()).toBe(tempValue);
+                            expect(leap[key](tempValue + 100)).not.toBe(tempValue);
+                            expect(leap[key](tempValue + 100)).toBe(tempValue + 100);
+                            expect(leap[key]()).toBe(tempValue + 100);
+                        }));
+
+                    });
+
+                }
+            }
+
+
+        });
 
     });
-})
+
+    describe('configured instance', function () {
+        beforeEach(module('angularLeap', function (leapProvider) {
+            for (var key in testBasicConfiguration) {
+                if (testBasicConfiguration.hasOwnProperty(key)) {
+                    var value = testBasicConfiguration[key];
+                    leapProvider[key](value + 1);
+                }
+            }
+        }));
+
+        for (var key in testBasicConfiguration) {
+            if (testBasicConfiguration.hasOwnProperty(key)) {
+                var value = testBasicConfiguration[key];
+                describe('key: ' + key, function () {
+                    it('should define a default timeout function', inject(function (leap) {
+                        expect(leap[key]).toBeDefined();
+                    }));
+
+                    it('should define a default timeout at ' + value + '+ 1', inject(function (leap) {
+                        expect(leap[key]()).toBe(value + 1);
+                    }));
+                });
+
+            }
+        }
+    });
+
+
+});
+
+
+
+
